b0ccff90f93fd86b48b985cc818fdbaf52d79a39
Mohd Abuzar <tech.mabuzar@gmail.com>
2026-03-17 00:47:30 +0530

    Refactor load_pdf to load multiple PDFs from folder

--- src/pdf_loader.py
+++ src/pdf_loader.py
@@ -1,13 +1,24 @@
+import os
 from langchain_community.document_loaders import PyPDFLoader
 
 
-def load_pdf(file_path):
+def load_pdfs(data_path="data"):
     """
-    Load a PDF file and convert it into LangChain document objects.
+    Load all PDF files from the data folder.
     """
 
-    loader = PyPDFLoader(file_path)
+    documents = []
 
-    documents = loader.load()
+    for file in os.listdir(data_path):
+
+        if file.endswith(".pdf"):
+
+            file_path = os.path.join(data_path, file)
+
+            loader = PyPDFLoader(file_path)
+
+            docs = loader.load()
+
+            documents.extend(docs)
 
     return documents
