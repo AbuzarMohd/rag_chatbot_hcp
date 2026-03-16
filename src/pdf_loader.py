3e6ced552d4b772c6aa5fb0722146e5c8cae46c9
Mohd Abuzar <tech.mabuzar@gmail.com>
2026-03-17 00:21:05 +0530

    Implement PDF loading functionality
    Add a function to load PDF files using PyPDFLoader

--- src/pdf_loader.py
+++ src/pdf_loader.py
@@ -1,1 +1,13 @@
+from langchain_community.document_loaders import PyPDFLoader
 
+
+def load_pdf(file_path):
+    """
+    Load a PDF file and convert it into LangChain document objects.
+    """
+
+    loader = PyPDFLoader(file_path)
+
+    documents = loader.load()
+
+    return documents
