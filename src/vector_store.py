63fa3b40889e1be79d5a982293472dcedd384be0
Mohd Abuzar <tech.mabuzar@gmail.com>
2026-03-17 01:12:18 +0530

    Update vector_store.py

--- src/vector_store.py
+++ src/vector_store.py
@@ -1,13 +1,15 @@
 from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 
-def create_vector_store(chunks, api_key):
+def create_vector_store(chunks):
     """
     Create a vector database from document chunks.
     """
 
-    embeddings = OpenAIEmbeddings(api_key=api_key)
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
 
     vectorstore = Chroma.from_documents(
         documents=chunks,
