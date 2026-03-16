c2c19d8752b521c7a63799f80ffd1a7ac8d94f5b
Mohd Abuzar <tech.mabuzar@gmail.com>
2026-03-17 00:23:12 +0530

    Update vector_store.py

--- src/vector_store.py
+++ src/vector_store.py
@@ -1,1 +1,17 @@
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
 
+
+def create_vector_store(chunks, api_key):
+    """
+    Create a vector database from document chunks.
+    """
+
+    embeddings = OpenAIEmbeddings(api_key=api_key)
+
+    vectorstore = Chroma.from_documents(
+        documents=chunks,
+        embedding=embeddings
+    )
+
+    return vectorstore
