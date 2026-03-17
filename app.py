61c99b16fe15a912b08fb6b20dc4e67da572f879
Mohd Abuzar <tech.mabuzar@gmail.com>
2026-03-18 01:21:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,35 @@
 import streamlit as st
-
 from src.pdf_loader import load_pdfs
 from src.text_splitter import split_documents
 from src.vector_store import create_vector_store
 from src.retriever import get_retriever
 from src.rag_chain import create_rag_chain
 
-
 st.title("PDF RAG Chatbot")
 
-# Get Google API key
 api_key = st.secrets.get("GOOGLE_API_KEY")
+
+if not api_key:
+    st.error("GOOGLE_API_KEY not found in secrets!")
+    st.stop()
 
 @st.cache_resource
 def initialize_rag():
-
     documents = load_pdfs("data")
-
     chunks = split_documents(documents)
-
     vectorstore = create_vector_store(chunks)
-
     retriever = get_retriever(vectorstore)
-
     qa_chain = create_rag_chain(retriever, api_key)
-
     return qa_chain
-
 
 qa_chain = initialize_rag()
 
 query = st.text_input("Ask a question from the PDFs")
 
 if query:
-    response = qa_chain.invoke({"query": query})
-    st.write(response["result"])
+    with st.spinner("Thinking..."):
+        try:
+            response = qa_chain.invoke({"query": query})
+            st.write(response["result"])
+        except Exception as e:
+            st.error(f"Error: {e}")
