8c8b591a815b126723c95802c6c6f3fb7e7a3ca6
Mohd Abuzar <tech.mabuzar@gmail.com>
2026-03-18 00:50:07 +0530

    Update vector_store.py

--- src/vector_store.py
+++ src/vector_store.py
@@ -1,19 +1,12 @@
 from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import HuggingFaceEmbeddings
-
+from langchain_huggingface import HuggingFaceEmbeddings  # ✅ updated import
 
 def create_vector_store(chunks):
-    """
-    Create a vector database from document chunks.
-    """
-
     embeddings = HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
-
     vectorstore = Chroma.from_documents(
         documents=chunks,
         embedding=embeddings
     )
-
     return vectorstore
